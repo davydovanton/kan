98abaa86d0907feaf183181fc9a396dd25c890ff
Bernie Chiu <bernie_chiu@hotmail.com>
2018-02-11 11:06:34 +0800

    Spin up test cases for RSpec matcher with payload targets

--- spec/abilities/application_spec.rb
+++ spec/abilities/application_spec.rb
@@ -4,15 +4,21 @@ RSpec.describe Kan::Application, type: :ability do
   class PostAbilities
     include Kan::Abilities
 
-    register('read') { |_| true }
-    register('edit') { |_, _| false }
+    register(:read) { |_| true }
+    register(:edit) { |user, post| user.id == post.user_id }
   end
 
   class UserAbilities
     include Kan::Abilities
 
-    register('read') { false }
+    register(:read) { |_| true }
+    register(:delete) { |user| user.admin? }
   end
+
+  let(:user)       { double('User', id: 1, admin?: false) }
+  let(:admin)      { double('User', id: 2, admin?: true) }
+  let(:post)       { double('Post', user_id: user.id) }
+  let(:other_post) { double('Post', user_id: admin.id) }
 
   subject do
     Kan::Application.new(
@@ -22,16 +28,27 @@ RSpec.describe Kan::Application, type: :ability do
   end
 
   permissions do
-    it "denies access for post edit" do
-      expect(subject).not_to permit('post.edit')
+    describe "Post Abilities" do
+      it "grant all access for read" do
+        expect(subject).to permit('post.read', user, post)
+        expect(subject).to permit('post.read', user, other_post)
+      end
+
+      it "grant owner access for edit" do
+        expect(subject).to permit('post.edit', user, post)
+        expect(subject).not_to permit('post.edit', user, other_post)
+      end
     end
 
-    it "grants access for post read" do
-      expect(subject).to permit('post.read')
-    end
+    describe "User Abilities" do
+      it "grant all users access for read" do
+        expect(subject).to permit('user.read', user)
+      end
 
-    it "denies access for user read" do
-      expect(subject).not_to permit('user.read')
+      it "deny normal user access for delete" do
+        expect(subject).not_to permit('user.delete', user)
+        expect(subject).to permit('user.delete', admin)
+      end
     end
   end
 end
