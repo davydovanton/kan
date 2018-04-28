d1af4d9a5a42c1479e6c46d0550b650d5a3cad9a
Anton Davydov <antondavydov.o@gmail.com>
2018-04-28 14:44:33 +0300

    Allow to detect roles for abilities object and scope

--- lib/kan/abilities_list.rb
+++ lib/kan/abilities_list.rb
@@ -1,14 +1,28 @@
 module Kan
   class AbilitiesList
+    ROLES_DETECT = 'roles'
+
     def initialize(name, list)
       @name = name
       @list = list
     end
 
     def call(*payload)
+      @name == ROLES_DETECT ? mapped_roles(payload) : ability_check(payload)
+    end
+
+  private
+
+    def ability_check(payload)
       @list
         .select { |abilities| abilities.class.valid_role?(*payload) }
         .any? { |abilities| abilities.ability(@name).call(*payload) }
+    end
+
+    def mapped_roles(payload)
+      @list.map do |abilities|
+        abilities.class.valid_role?(*payload) ? abilities.class.role_name : nil
+      end.compact
     end
   end
 end
